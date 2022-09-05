77481f152343a9a895e666925fd33c0dec465483
Enzo Gebauer <80331520+enzogebauer@users.noreply.github.com>
2022-09-05 13:52:20 -0300

    almost fineshed

--- script.js
+++ script.js
@@ -10,9 +10,14 @@ const jump = ()=>{
 }
 const loop = setInterval(() =>{
   const pipePosition = pipe.offsetLeft;
-  if(pipePosition <= 120){
+  const marioPosition =   +window.getComputedStyle(mario).bottom.replace('px', '');
+  let cont = 0
+  if(pipePosition <= 120 && pipePosition > 0 && marioPosition<80){
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
   }
-},10);
+  
+},10); 
 document.addEventListener('keydown',jump);
